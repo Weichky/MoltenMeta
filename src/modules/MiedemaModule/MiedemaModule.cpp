3c5e8d58f863e098b8848ccaa97f8161215219d1
Weichky Charl <weichkycharl@gmail.com>
2026-03-30 17:50:05 +0800

    feat(modules): add Miedema module for enthalpy of mixing calculations and update CMake configuration

---
+++ src/modules/MiedemaModule/MiedemaModule.cpp
@@ -0,0 +1,125 @@
+/**
+ * Miedema module for calculating enthalpy of mixing.
+ * 
+ * Input:
+ *   - x_A: mole fraction of element A [0, 1]
+ *   - elemA/elemB: ElementProperties {V_23, phi, n_WS_13, mu}
+ *   - miedemaConst: {a, p, q, r_over_p}
+ * 
+ * Output: ΔH_AB in J/mol (per mole of alloy)
+ * 
+ * Ref: Miedema et al., Philips Tech. Rev. (1973)
+
+
+ * Original formula in LaTeX: 
+ * $$
+ * \Delta H_{AB}=f_{AB}\frac{x_Ax_B\left[1+\mu_Ax_B\left(\phi_A-\phi_B\right)\right]\left[1+\mu_Bx_A\left(\phi_B-\phi_A\right)\right]}{x_AV_{A}^{2/3}\left[1+\mu_Ax_B\left(\phi_A-\phi_B\right)\right]+x_BV_{B}^{2/3}\left[1+\mu_Bx_A\left(\phi_B-\phi_A\right)\right]}
+ * $$
+ * where:
+ * $$
+ * f_{AB}=2pV_A^{2/3}V_B^{2/3}\frac{\frac{q}{p}\left(\Delta n^{1/3}_{WS}\right)^2-\left(\Delta\phi\right)^2-a\frac{r}{p}}{\left(n^{1/3}_{WS}\right)^{-1}_A+\left(n^{1/3}_{WS}\right)^{-1}_B}
+ * $$
+**/
+
+# include <pybind11/pybind11.h>
+# include <pybind11/numpy.h>
+# include <cmath>
+
+struct ElementProperties
+{
+    double V_23;
+    double phi;
+    double n_WS_13;
+    double mu;
+};
+
+struct MiedemaConst
+{
+    double a;
+    double p;
+    double q;
+    double r_over_p;
+};
+
+namespace py = pybind11;
+class MiedemaModule {
+    public:
+        MiedemaModule(ElementProperties elemA, ElementProperties elemB, MiedemaConst miedemaConst)
+            : elemA(elemA), elemB(elemB), miedemaConst(miedemaConst) {
+            precompute();
+        }
+
+        double calculateSingle(double x_A) const{
+            const double x_B = 1 - x_A;
+            double numerator = x_A * x_B * (1 + elemA.mu * x_B * delta_phi) * (1 - elemB.mu * x_A * delta_phi);
+            double denominator = x_A * elemA.V_23 * (1 + elemA.mu * x_B * delta_phi)
+                + x_B * elemB.V_23 * (1 - elemB.mu * x_A * delta_phi);
+
+            return f_AB * numerator / denominator;
+        }
+
+        py::array_t<double> calculateRange(double x_A_start, double x_A_end, int num) const {
+            py::array_t<double> result = py::array_t<double>(num);
+            py::buffer_info buf = result.request();
+
+            double *ptr = static_cast<double*>(buf.ptr);
+
+            double step = (num > 1)
+                ? (x_A_end - x_A_start) / (num - 1)
+                : 0;
+
+            for (int i = 0; i < num; ++i) {
+                double x_A = x_A_start + i * step;
+
+                if (i == num - 1) x_A = x_A_end;
+                ptr[i] = calculateSingle(x_A);
+            }
+
+            return result;
+        }
+
+    private:
+        ElementProperties elemA, elemB;
+        MiedemaConst miedemaConst;
+
+        double f_AB;
+        double delta_phi;
+
+        void precompute() {
+            // Calculate f_AB based on the provided properties and constants
+
+            // Define delta_phi as the difference in electronegativity between A and B
+            // Thus, phi_B - phi_A = - delta_phi
+            delta_phi = elemA.phi - elemB.phi;
+
+            double delta_n_WS_13 = elemA.n_WS_13 - elemB.n_WS_13;
+
+            double pre_factor = 2 * miedemaConst.p * elemA.V_23 * elemB.V_23;
+            double numerator = miedemaConst.q / miedemaConst.p 
+                * delta_n_WS_13 * delta_n_WS_13 - delta_phi * delta_phi - miedemaConst.a * miedemaConst.r_over_p;
+
+            double denominator =1 / elemA.n_WS_13 + 1 / elemB.n_WS_13;
+            // Calculate f_AB
+            f_AB = pre_factor * numerator / denominator;
+        }
+
+};
+
+PYBIND11_MODULE(MiedemaModule, m) {
+    py::class_<ElementProperties>(m, "ElementProperties")
+        .def(py::init<double, double, double, double>())
+        .def_readwrite("V_23", &ElementProperties::V_23)
+        .def_readwrite("phi", &ElementProperties::phi)
+        .def_readwrite("n_WS_13", &ElementProperties::n_WS_13)
+        .def_readwrite("mu", &ElementProperties::mu);
+    py::class_<MiedemaConst>(m, "MiedemaConst")
+        .def(py::init<double, double, double, double>())
+        .def_readwrite("a", &MiedemaConst::a)
+        .def_readwrite("p", &MiedemaConst::p)
+        .def_readwrite("q", &MiedemaConst::q)
+        .def_readwrite("r_over_p", &MiedemaConst::r_over_p);
+    py::class_<MiedemaModule>(m, "MiedemaModule")
+        .def(py::init<ElementProperties, ElementProperties, MiedemaConst>())
+        .def("calculateSingle", &MiedemaModule::calculateSingle)
+        .def("calculateRange", &MiedemaModule::calculateRange);
+}
